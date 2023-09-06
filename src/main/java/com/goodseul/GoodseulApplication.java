b494a23130c1697b257917a9eaf886ecf263e7eb
kddongkyu <113885745+kddongkyu@users.noreply.github.com>
2023-09-06 11:15:43 +0900

    first commit

---
+++ src/main/java/com/goodseul/GoodseulApplication.java
@@ -0,0 +1,13 @@
+package com.goodseul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodseulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodseulApplication.class, args);
+    }
+
+}

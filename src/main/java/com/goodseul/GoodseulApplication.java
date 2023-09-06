b7dee63e0a957304b8822680a20f383786df48ee
kddongkyu <113885745+kddongkyu@users.noreply.github.com>
2023-09-06 11:19:58 +0900

    프로젝트 생성

--- src/main/java/com/goodseul/GoodseulApplication.java
+++ src/main/java/com/goodseul/GoodseulApplication.java
@@ -2,8 +2,14 @@ package com.goodseul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "data")
+@EnableJpaRepositories(basePackages = "data.repository")
+@EntityScan(basePackages = "data.entity")
 public class GoodseulApplication {
 
     public static void main(String[] args) {

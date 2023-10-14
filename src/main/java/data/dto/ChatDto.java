106985f9f98a3755bfa4073b4495655d6e9b21fe
KimDongkyu <dongkyu95@gmail.com>
2023-10-14 23:16:07 +0900

    feat : chat 기능 구현완료, 출석체크 API 랜덤 점수 메서드 수정

--- src/main/java/data/dto/ChatDto.java
+++ src/main/java/data/dto/ChatDto.java
@@ -19,10 +19,20 @@ public class ChatDto {
     private String message;
     private String type;
     private Timestamp time;
+    private String roomId;
+
+    @Builder
+    public ChatDto(int sender, int receiver, String message, Timestamp time, String roomId) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.message = message;
+        this.time = time;
+        this.roomId = roomId;
+    }
 
 
     public ChatEntity convertToEntity(ChatDto chatDto) {
-        return new ChatEntity(chatDto.getSender(), chatDto.getReceiver(), chatDto.getMessage(), chatDto.getTime());
+        return new ChatEntity(chatDto.getSender(), chatDto.getReceiver(), chatDto.getMessage(), new Timestamp(System.currentTimeMillis()), false, chatDto.getRoomId());
     }
 
 }
